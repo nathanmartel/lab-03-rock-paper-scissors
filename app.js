5e6b02363a653537751dfadaf0a5dfc7ecd3d468
Scott Campbell <stc9486@gmail.com>
2020-01-17 11:12:35 -0800

    send

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ import getRandomThrow from './get-random-throw.js';
 // Get DOM
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
-let userThrow = document.querySelector('input:checked');
 const computerThrowText = document.getElementById('computer-throw-text');
 const computerThrowImage = document.getElementById('computer-throw-image');
 const totalWins = document.getElementById('total-results-wins');
 const totalLosses = document.getElementById('total-results-losses');
 const totalDraws = document.getElementById('total-results-draws');
+
+
 
 // Initialize Global vars
 let wins;
@@ -25,9 +26,10 @@ resetButton.addEventListener('click', initializeGame);
 
 // Change state and Update DOM
 function playGame() {
+    let userThrow = document.querySelector('input:checked');
     const computerThrow = getRandomThrow();
-    // console.log('Computer throw: ', computerThrow);
-    // console.log('User throw: ', userThrow.value);
+    console.log('Computer throw: ', computerThrow);
+    console.log('User throw: ', userThrow.value);
     computerThrowText.textContent = computerThrow;
     if (computerThrow === 'rock') { 
         computerThrowImage.src = './images/rock.png';
@@ -38,14 +40,23 @@ function playGame() {
     }
     
     const result = checkResult(userThrow.value, computerThrow);
+    //console.log(computerThrow);
+    //console.log(userThrow)
+    totalWins.style.color = 'white';
+    totalLosses.style.color = 'white';
+    totalDraws.style.color = 'white';
+
     if (result === 'win') { 
         wins++;
+        totalWins.style.color = 'green';
         totalWins.textContent = wins;
     } else if (result === 'lose') { 
         losses++;
+        totalLosses.style.color = 'red'; 
         totalLosses.textContent = losses;
     } else {
         draws++;
+        totalDraws.style.color = 'grey';
         totalDraws.textContent = draws;
     }
 }
